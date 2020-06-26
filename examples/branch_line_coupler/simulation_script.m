34e131f17837b834216beeb2a78ff3b1595a59d3
Leandro <leoheck@gmail.com>
2020-06-26 16:00:21 -0300

    Disable initial clear screen, format parenteses and equations better

--- examples/branch_line_coupler/simulation_script.m
+++ examples/branch_line_coupler/simulation_script.m
@@ -4,7 +4,7 @@
 
 close all
 clear
-clc
+% clc
 
 % Skip FDTD and analyze data if previously generated
 post_proc_only = 0;
@@ -32,16 +32,16 @@ unit = 1e-3; % all length in mm
 % frequency range of interest
 f_start = 1e9;
 f_stop  = 4e9;
-f0 = 0.5 * (f_start+f_stop);
+f0 = 0.5 * (f_start + f_stop);
 fc = f_stop - f0;
 
 % Setup exitation types
-FDTD = InitFDTD( 'NrTs', 40000, 'EndCriteria', 1e-3 );
+FDTD = InitFDTD('NrTs', 40000, 'EndCriteria', 1e-3);
 FDTD = SetGaussExcite(FDTD, f0, f_stop - f0);
 
 % boundary conditions
 BC = {'MUR' 'MUR' 'MUR' 'MUR' 'MUR' 'MUR'};
-FDTD = SetBoundaryCond( FDTD, BC );
+FDTD = SetBoundaryCond(FDTD, BC);
 
 % Setup CSXCAD geometry & mesh
 CSX = InitCSX();
@@ -73,13 +73,13 @@ stop  = [1.75 -25.95 1];
 
 % Setup materials used (WARNING: check that material names are same in config.json)
 CSX = AddMaterial(CSX, 'pcb');
-CSX = SetMaterialProperty( CSX, 'pcb', 'Epsilon', 4.2, 'Mue', 1, 'Kappa', 0, 'Sigma', 0, 'Density', 1 );
+CSX = SetMaterialProperty(CSX, 'pcb', 'Epsilon', 4.2, 'Mue', 1, 'Kappa', 0, 'Sigma', 0, 'Density', 1);
 CSX = AddMetal(CSX, 'metal_top');
 CSX = AddMetal(CSX, 'metal_bot');
 CSX = AddMaterial(CSX, 'drill_fill');
-CSX = SetMaterialProperty( CSX, 'drill_fill', 'Epsilon', 1, 'Mue', 1, 'Kappa', 0, 'Sigma', 0, 'Density', 1 );
+CSX = SetMaterialProperty(CSX, 'drill_fill', 'Epsilon', 1, 'Mue', 1, 'Kappa', 0, 'Sigma', 0, 'Density', 1);
 CSX = AddMaterial(CSX, 'box_material');
-CSX = SetMaterialProperty( CSX, 'box_material', 'Epsilon', 1, 'Mue', 1, 'Kappa', 0, 'Sigma', 0, 'Density', 1 );
+CSX = SetMaterialProperty(CSX, 'box_material', 'Epsilon', 1, 'Mue', 1, 'Kappa', 0, 'Sigma', 0, 'Density', 1);
 
 % load model in CSX structure (model script is output from pcbmodelgen)
 CSX = kicad_pcb_model(CSX);
@@ -92,10 +92,10 @@ CSX = DefineRectGrid(CSX, unit, model_mesh);
 
 
 if(add_field_dump)
-    CSX = AddDump( CSX, 'Et_', 'DumpType', 0, 'DumpMode', 0); % cell interpolated
+    CSX = AddDump(CSX, 'Et_', 'DumpType', 0, 'DumpMode', 0); % cell interpolated
     start = [ 0.0   0.0   0.5];
     stop  = [34.5 -29.5   0.5];
-    CSX = AddBox( CSX, 'Et_', 15, start, stop );
+    CSX = AddBox(CSX, 'Et_', 15, start, stop);
 end
 
 
@@ -105,20 +105,20 @@ disp('Model import and simulation setup done');
 Sim_Path = './tmp';
 Sim_CSX = 'simulation.xml';
 
-if(post_proc_only==0)
-    [status, message, messageid] = rmdir( Sim_Path, 's' ); % clear previous directory
-    [status, message, messageid] = mkdir( Sim_Path ); % create empty simulation folder
+if(post_proc_only == 0)
+    [status, message, messageid] = rmdir(Sim_Path, 's'); % clear previous directory
+    [status, message, messageid] = mkdir(Sim_Path); % create empty simulation folder
 
     disp('Generating simulation configuration file');
 
     % write openEMS compatible xml-file
-    WriteOpenEMS( [Sim_Path '/' Sim_CSX], FDTD, CSX );
+    WriteOpenEMS([Sim_Path '/' Sim_CSX], FDTD, CSX);
     disp('Done');
 
     disp('Showing geometry');
 
     % show the structure
-    CSXGeomPlot( [Sim_Path '/' Sim_CSX] );
+    CSXGeomPlot([Sim_Path '/' Sim_CSX]);
 
     if(show_model_only)
         disp('Showing only model - exit');
@@ -133,7 +133,7 @@ if(post_proc_only==0)
     disp('Starting openEMS');
 
     % run openEMS
-    RunOpenEMS( Sim_Path, Sim_CSX, cmd_params);
+    RunOpenEMS(Sim_Path, Sim_CSX, cmd_params);
 
     if(fdtd_preproc_only)
         disp('Only preprocessing - exit');
@@ -145,42 +145,42 @@ end
 % Do post processing as you normaly would with openEMS and Plots
 % ===========================================================================================
 
-freq = linspace( max([1e9,f0-fc]), f0+fc, 501 );
+freq = linspace(max([1e9, f0-fc]), f0 + fc, 501);
 
-U = ReadUI( {'port_ut1','port_ut2','port_ut3','port_ut4','et'}, [Sim_Path '/'], freq ); % time domain/freq domain voltage
-I = ReadUI( {'port_it1','port_it2','port_it3','port_it4'}, [Sim_Path '/'], freq ); % time domain/freq domain current (half time step is corrected)
+U = ReadUI({'port_ut1','port_ut2','port_ut3','port_ut4','et'}, [Sim_Path '/'], freq); % time domain/freq domain voltage
+I = ReadUI({'port_it1','port_it2','port_it3','port_it4'}, [Sim_Path '/'], freq); % time domain/freq domain current (half time step is corrected)
 
 % Plot time domain voltage
 figure
-[ax,h1,h2] = plotyy( U.TD{1}.t/1e-9, U.TD{1}.val, U.TD{5}.t/1e-9, U.TD{5}.val );
-set( h1, 'Linewidth', 2 );
-set( h1, 'Color', [1 0 0] );
-set( h2, 'Linewidth', 2 );
-set( h2, 'Color', [0 0 0] );
+[ax,h1,h2] = plotyy(U.TD{1}.t / 1e-9, U.TD{1}.val, U.TD{5}.t / 1e-9, U.TD{5}.val);
+set(h1, 'Linewidth', 2);
+set(h1, 'Color', [1 0 0]);
+set(h2, 'Linewidth', 2);
+set(h2, 'Color', [0 0 0]);
 grid on
-title( 'time domain voltage' );
-xlabel( 'time t / ns' );
-ylabel( ax(1), 'voltage ut1 / V' );
-ylabel( ax(2), 'voltage et / V' );
+title('time domain voltage');
+xlabel('time t / ns');
+ylabel(ax(1), 'voltage ut1 / V');
+ylabel(ax(2), 'voltage et / V');
 
 % Now make the y-axis symmetric to y=0 (align zeros of y1 and y2)
 y1 = ylim(ax(1));
 y2 = ylim(ax(2));
-ylim( ax(1), [-max(abs(y1)) max(abs(y1))] );
-ylim( ax(2), [-max(abs(y2)) max(abs(y2))] );
+ylim(ax(1), [-max(abs(y1)) max(abs(y1))]);
+ylim(ax(2), [-max(abs(y2)) max(abs(y2))]);
 print -dpng Ut.png
 
 % Plot feed point impedance
 figure
 Zin = U.FD{1}.val ./ I.FD{1}.val;
-plot( freq/1e6, real(Zin), 'k-', 'Linewidth', 2 );
+plot(freq/1e6, real(Zin), 'k-', 'Linewidth', 2);
 hold on
 grid on
-plot( freq/1e6, imag(Zin), 'r--', 'Linewidth', 2 );
-title( 'feed point impedance' );
-xlabel( 'frequency f / MHz' );
-ylabel( 'impedance Z_{in} / Ohm' );
-legend( 'real', 'imag' );
+plot(freq/1e6, imag(Zin), 'r--', 'Linewidth', 2);
+title('feed point impedance');
+xlabel('frequency f / MHz');
+ylabel('impedance Z_{in} / Ohm');
+legend('real', 'imag');
 print -dpng Z.png
 
 % Plot some S parameters
@@ -201,11 +201,11 @@ U4 = U.FD{4}.val;
 s41 = (2 * U4) ./ U0;
 log_s41 = 20*log10(abs(s41));
 
-freq_MHz = freq/1e6;
+freq_MHz = freq / 1e6;
 
 figure
-plot( freq_MHz, log_s11, 'k;S11;', freq_MHz, log_s21, 'r;S21;', freq_MHz, log_s31, 'g;S31;', freq_MHz, log_s41, 'b;S41;');
+plot(freq_MHz, log_s11, 'k;S11;', freq_MHz, log_s21, 'r;S21;', freq_MHz, log_s31, 'g;S31;', freq_MHz, log_s41, 'b;S41;');
 grid on
-title( 'S parameters' );
-xlabel( 'frequency f / MHz' );
+title('S parameters');
+xlabel('frequency f / MHz');
 print -dpng S_params.png
